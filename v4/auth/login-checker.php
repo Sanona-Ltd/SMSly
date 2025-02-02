93e2c53ff0adc53519aceabdff35c8b3d931a114
Florin-schildi <duduletsplay@outlook.com>
2025-02-02 17:08:58 +0100

    Update login checker global variable naming convention

--- v4/auth/login-checker.php
+++ v4/auth/login-checker.php
@@ -6,26 +6,26 @@ if (isset($_SESSION["user_id"]) && !empty($_SESSION["user_id"])) {
     // Benutzer ist angemeldet - Globale Variablen setzen
     
     // User-Daten
-    $GLOBALS["GLOBAL_USER_ID"] = $_SESSION["user_id"];
-    $GLOBALS["GLOBAL_USER_EMAIL"] = $_SESSION["email"];
+    $GLOBALS["GLOBAL_VARIABLE_USER_ID"] = $_SESSION["user_id"];
+    $GLOBALS["GLOBAL_VARIABLE_USER_EMAIL"] = $_SESSION["email"];
     
     // Namen
-    $GLOBALS["GLOBAL_USER_USERNAME"] = $_SESSION["username"];
-    $GLOBALS["GLOBAL_USER_NAME"] = $_SESSION["name"];
-    $GLOBALS["GLOBAL_USER_SURNAME"] = $_SESSION["surname"];
+    $GLOBALS["GLOBAL_VARIABLE_USER_USERNAME"] = $_SESSION["username"];
+    $GLOBALS["GLOBAL_VARIABLE_USER_NAME"] = $_SESSION["name"];
+    $GLOBALS["GLOBAL_VARIABLE_USER_SURNAME"] = $_SESSION["surname"];
     
     // Adresse
-    $GLOBALS["GLOBAL_USER_STREET"] = $_SESSION["street"];
-    $GLOBALS["GLOBAL_USER_NUMBER"] = $_SESSION["number"];
-    $GLOBALS["GLOBAL_USER_ZIPCODE"] = $_SESSION["zip_code"];
-    $GLOBALS["GLOBAL_USER_CITY"] = $_SESSION["city"];
-    $GLOBALS["GLOBAL_USER_COUNTRY"] = $_SESSION["country"];
+    $GLOBALS["GLOBAL_VARIABLE_USER_STREET"] = $_SESSION["street"];
+    $GLOBALS["GLOBAL_VARIABLE_USER_NUMBER"] = $_SESSION["number"];
+    $GLOBALS["GLOBAL_VARIABLE_USER_ZIPCODE"] = $_SESSION["zip_code"];
+    $GLOBALS["GLOBAL_VARIABLE_USER_CITY"] = $_SESSION["city"];
+    $GLOBALS["GLOBAL_VARIABLE_USER_COUNTRY"] = $_SESSION["country"];
     
     // Sicherheit & Berechtigungen
-    $GLOBALS["GLOBAL_USER_CANLOGIN"] = $_SESSION["can_login"];
-    $GLOBALS["GLOBAL_USER_SMSCONTINGENT"] = $_SESSION["sms_contingent"];
-    $GLOBALS["GLOBAL_USER_OWNSENDER"] = $_SESSION["own_sender"];
-    $GLOBALS["GLOBAL_USER_RANK"] = $_SESSION["rank"];
+    $GLOBALS["GLOBAL_VARIABLE_USER_CANLOGIN"] = $_SESSION["can_login"];
+    $GLOBALS["GLOBAL_VARIABLE_USER_SMSCONTINGENT"] = $_SESSION["sms_contingent"];
+    $GLOBALS["GLOBAL_VARIABLE_USER_OWNSENDER"] = $_SESSION["own_sender"];
+    $GLOBALS["GLOBAL_VARIABLE_USER_RANK"] = $_SESSION["rank"];
 
 } else {
     // Benutzer ist nicht angemeldet
