2c81aa018039191be07df036276e2a50e9a32c00
Florin-schildi <duduletsplay@outlook.com>
2025-02-05 00:05:20 +0100

    Update TitanPay webhook to update user SMS contingent and payment status

--- v4/app/payments/titanpay-webhook.php
+++ v4/app/payments/titanpay-webhook.php
@@ -12,13 +12,23 @@ $message = $data['message'] ?? null;
 $payment_id = $data['payment_id'] ?? null;
 $payment_method = $data['payment_method'] ?? null;
 
+$payment_date = date('Y-m-d H:i:s');
+
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "https://api.titanpay.de/payments/" . $payment_id);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_HTTPHEADER, [
-    "Authorization: Bearer YOUR_API_KEY",
-    "Content-Type: application/json"
-]);
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://db.sanona.org/api/b872c5a521a44cc0983443494237e81e/payments?where[payment_id]=' . $payment_id . '&timestamps=null',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_HTTPHEADER => array(
+        'Authorization: Bearer hYNIyTLFG1eHQ2ap146I3ENmZ6Ct6OpsghpyySOB'
+    ),
+));
 
 $response = curl_exec($curl);
 curl_close($curl);
@@ -32,12 +42,36 @@ if (empty($payment_data)) {
 } else {
     // Zahlung wurde gefunden, speichere die ID in einer Variable
     $payment_database_id = $payment_data[0]['id'];
-
-    // Benutzer-ID auslesen
+    $contingent = $payment_data[0]['contingent'];
     $user_id = $payment_data[0]['user']['id'];
+    $sms_contingent = $payment_data[0]['user']['sms_contingent'];
 
-    //aktuelles Datum und Uhrzeit
-    $payment_date = date('Y-m-d H:i:s');
+
+
+    $new_sms_contingent = $sms_contingent + $contingent;
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://db.sanona.org/api/b872c5a521a44cc0983443494237e81e/user/update/' . $user_id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array('sms_contingent' => $new_sms_contingent),
+        CURLOPT_HTTPHEADER => array(
+            'Accept: application/json',
+            'Authorization: Bearer hYNIyTLFG1eHQ2ap146I3ENmZ6Ct6OpsghpyySOB'
+        ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    // echo $response;
 
     $curl = curl_init();
 
@@ -56,7 +90,5 @@ if (empty($payment_data)) {
     $response = curl_exec($curl);
 
     curl_close($curl);
-    echo $response;
+    // echo $response;
 }
-
-
