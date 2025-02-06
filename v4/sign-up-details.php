f7254ad8029d20b771cbd3ad7a294521c9044f57
Florin-schildi <duduletsplay@outlook.com>
2025-02-06 07:18:57 +0100

    Update sign-up details form with place, zip code, and country fields

--- v4/sign-up-details.php
+++ v4/sign-up-details.php
@@ -84,23 +84,30 @@
                                                     class="form-control" spellcheck="false" data-ms-editor="true">
                                             </div>
                                         </div>
+                                        <div class="d-flex flex-sm-row flex-column gap-sm-7 gap-3">
+                                            <div class="d-flex flex-column flex-grow-1 gap-2">
+                                                <label for="Fname" class="fs-3 fw-semibold text-dark">
+                                                    Place *
+                                                </label>
+                                                <input type="text" name="Fname" id="Fname" placeholder="First Name"
+                                                    class="form-control" spellcheck="false" data-ms-editor="true">
+                                            </div>
+                                            <div class="d-flex flex-column flex-grow-1 gap-2">
+                                                <label for="Lname" class="fs-3 fw-semibold text-dark">
+                                                    Zip Code *
+                                                </label>
+                                                <input type="text" name="Lname" id="Lname" placeholder="Last name"
+                                                    class="form-control" spellcheck="false" data-ms-editor="true">
+                                            </div>
+                                        </div>
                                         <div class="d-flex flex-column gap-2">
-                                            <label for="enquire" class="fs-3 fw-semibold text-dark">Enquire related
-                                                to *</label>
+                                            <label for="enquire" class="fs-3 fw-semibold text-dark">Country *</label>
                                             <select class="form-select w-auto">
-                                                <option value="1">General Enquiry</option>
-                                                <option value="2">Customer Service Enquiry</option>
-                                                <option value="3">Legal Enquiry</option>
-                                                <option value="4">General Enquiry</option>
+                                                <option value="ch">Switzerland</option>
                                             </select>
                                         </div>
-                                        <div class="d-flex flex-column gap-2">
-                                            <label for="message" class="fs-3 fw-semibold text-dark">Message</label>
-                                            <textarea name="message" id="message" class="form-control" rows="5"
-                                                spellcheck="false" data-ms-editor="true"></textarea>
-                                        </div>
                                     </div>
-                                    <button class="btn btn-primary mt-sm-7 mt-3 px-9 py-6">Submit</button>
+                                    <button class="btn btn-primary mt-sm-7 mt-3 px-9 py-6">Save registration</button>
                                 </form>
                             </div>
                         </div>
