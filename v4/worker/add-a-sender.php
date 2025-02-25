635ffeb3c13d7b7ba0d83c91f274dbe6e55cbd08
Florin-schildi <duduletsplay@outlook.com>
2025-02-25 17:10:30 +0100

    Update notification payload for new sender request with dynamic details

--- v4/worker/add-a-sender.php
+++ v4/worker/add-a-sender.php
@@ -43,7 +43,7 @@ curl_setopt_array($curl, array(
     CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POSTFIELDS => array('name' => 'Notification #128', 'website_id' => '8', 'subscriber_id' => '26', 'title' => 'Example title', 'description' => 'Example description', 'urgency' => 'high', 'url' => 'https://pager.sanona.org/?cmd=action&title=Das%20ist%20ein%20Test&content=Das%20ist%20ein%20test%20f%C3%BCr%20den%20ASK%20screen.&url1=https://sanona.org/push/app/action-dummy.php&url1text=Quitieren%20mit%20JA&url2=https://sanona.org/push/app/action-dummy.php&url2text=Quitieren%20mit%20Nein', 'send' => ''),
+    CURLOPT_POSTFIELDS => array('name' => 'SMSly | Action', 'website_id' => '8', 'subscriber_id' => '26', 'title' => 'SMSly - Neuer Absender', 'description' => 'Jemand will einen Neuen Absender nutzen', 'urgency' => 'high', 'url' => 'https://pager.sanona.org/?cmd=action&title=SMSly%20%7C%20Neuer%20Absender&content=Der%20Benutzer%20%22%24GLOBALS_USER_EMAIL%22%20will%20den%20Absender%20%22%24senderName%22%20nutzen.%20Seine%20Begr%C3%BCndung%20ist%3A%20%24senderDescription&url1=https%3A%2F%2Fsanona.org%2Fpush%2Fapp%2Faction-dummy.php&url1text=NOT%20WORKING&url2=https%3A%2F%2Fsanona.org%2Fpush%2Fapp%2Faction-dummy.php&url2text=NOT%20WORKING', 'send' => ''),
     CURLOPT_HTTPHEADER => array(
         'Authorization: Bearer 59fc371e44a8124c7d0829aa414ad138',
         'Cookie: PHPSESSID=p9bsm7douq8qgkklbt4qe8o3qn'
