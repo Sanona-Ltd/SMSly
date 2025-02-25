4e5969609ed101567af1f04596519d06debc16a3
Florin-schildi <duduletsplay@outlook.com>
2025-02-25 20:55:18 +0100

    Add account movement logging for TitanPay webhook payment processing

--- public/titanpay-webhook.php
+++ public/titanpay-webhook.php
@@ -48,6 +48,7 @@ if (empty($payment_data)) {
 } else {
     // Zahlung wurde gefunden, speichere die ID in einer Variable
     $payment_database_id = $payment_data[0]['id'];
+    $product_name = $payment_data[0]['product_name'];
     $contingent = $payment_data[0]['contingent'];
     $user_id = $payment_data[0]['user']['id'];
     $sms_contingent = $payment_data[0]['user']['sms_contingent'];
@@ -113,8 +114,31 @@ if (empty($payment_data)) {
     }
 
     curl_close($curl);
-    
+
     // Erfolgreiche Verarbeitung
     http_response_code(200);
     sendLogAsync('titanpay.webhook', $payment_id, 'payment.processed', 'Payment processed successfully');
+
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://db.sanona.org/api/b872c5a521a44cc0983443494237e81e/account-movements',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array('relation' => $user_id, 'title' => $product_name . 'Paket', 'description' => 'SMSly', 'quantity' => $contingent, 'type' => 'positive'),
+        CURLOPT_HTTPHEADER => array(
+            'Accept: application/json',
+            'Authorization: Bearer hYNIyTLFG1eHQ2ap146I3ENmZ6Ct6OpsghpyySOB'
+        ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
 }
